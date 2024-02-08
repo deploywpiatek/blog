538b007393d5bdb100444ba48e34995058b0cf91
deploywpiatek <deploywpiatek@proton.me>
2024-02-09 00:07:34 +0100

    Init

---
+++ src/variables.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Deploy w piątek';
+export const SITE_DESCRIPTION = 'Jeśli widzisz ten opis to znaczy, że strona się dobrze wypozycjonowała. [Miejsce na Twoją reklamę?]. A tak w ogóle to o Frontendzie piszę. Pzdr!';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 12;
